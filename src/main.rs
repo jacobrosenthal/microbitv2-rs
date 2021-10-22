7f9fbb065f93f7d316fbca6768ae23512dc3292a
Jacob Rosenthal <jacobrosenthal@gmail.com>
2021-10-22 03:14:24 -0700

    update embassy, refactor, use gpiote for buttons

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,58 @@
-//! cargo flash --release
+//! cargo run --release
 
-#![no_std]
 #![no_main]
+#![no_std]
 #![feature(type_alias_impl_trait)]
-#![allow(incomplete_features)]
 
-use embassy_nrf as _;
+use defmt::{info, unwrap};
 use nrf_softdevice_defmt_rtt as _; // global logger
 use panic_probe as _;
 
-use core::mem;
-use cortex_m_rt::entry;
-use defmt::{info, unwrap};
-use embassy::executor::Executor;
-use embassy::traits::gpio::WaitForLow;
-use embassy::util::Forever;
-use embassy_nrf::gpio::{AnyPin, Input, Pin as _, Pull};
-use embassy_nrf::gpiote::PortInput;
-use embassy_nrf::interrupt::Priority;
-
-use futures::pin_mut;
-
+use embassy::executor::Spawner;
+use embassy_nrf::config::{Config, HfclkSource, LfclkSource};
+use embassy_nrf::gpio::{self, AnyPin, Pin};
+use embassy_nrf::gpiote::{self, AnyChannel, Channel, InputChannel};
+use embassy_nrf::{interrupt, Peripherals};
+use futures::FutureExt;
 use nrf_softdevice::ble::{gatt_server, peripheral};
 use nrf_softdevice::{raw, Softdevice};
 
-static EXECUTOR: Forever<Executor> = Forever::new();
-
-#[embassy::task]
-async fn softdevice_task(sd: &'static Softdevice) {
-    sd.run().await;
-}
-
-#[nrf_softdevice::gatt_server(uuid = "9e7312e0-2354-11eb-9f10-fbc30a62cf38")]
+// define a bluetooth service with one characteristic we can write and read to
+#[nrf_softdevice::gatt_service(uuid = "9e7312e0-2354-11eb-9f10-fbc30a62cf38")]
 struct FooService {
-    #[characteristic(uuid = "9e7312e0-2354-11eb-9f10-fbc30a63cf38", read, write, notify)]
+    #[characteristic(uuid = "9e7312e0-2354-11eb-9f10-fbc30a63cf38", read, write)]
     foo: u16,
 }
 
-async fn run_bluetooth(sd: &'static Softdevice, server: &FooService) {
+#[nrf_softdevice::gatt_server]
+struct Server {
+    foo: FooService,
+}
+
+#[embassy::main(config = "embassy_config()")]
+async fn main(spawner: Spawner, dp: Peripherals) {
+    info!("Hello World!");
+
+    let config = softdevice_config();
+    let sd = Softdevice::enable(&config);
+
+    let button1 = gpiote::InputChannel::new(
+        dp.GPIOTE_CH1.degrade(),
+        gpio::Input::new(dp.P0_14.degrade(), gpio::Pull::Up),
+        gpiote::InputChannelPolarity::HiToLo,
+    );
+
+    unwrap!(spawner.spawn(softdevice_task(sd)));
+    unwrap!(spawner.spawn(bluetooth_task(sd, button1)));
+}
+
+#[embassy::task]
+pub async fn bluetooth_task(
+    sd: &'static Softdevice,
+    button1: InputChannel<'static, AnyChannel, AnyPin>,
+) {
+    let server: Server = unwrap!(gatt_server::register(sd));
+
     #[rustfmt::skip]
     let adv_data = &[
         0x02, 0x01, raw::BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE as u8,
@@ -49,90 +64,57 @@ async fn run_bluetooth(sd: &'static Softdevice, server: &FooService) {
         0x03, 0x03, 0x09, 0x18,
     ];
 
-    loop {
-        let config = peripheral::Config::default();
+    let config = peripheral::Config::default();
+
+    info!("Bluetooth is OFF");
+    info!("Press microbit-v2 button 1 to enable, press again to disconnect");
+
+    'waiting: loop {
+        // wait here until button is pressed
+        button1.wait().await;
+
+        info!("advertising!");
+
         let adv = peripheral::ConnectableAdvertisement::ScannableUndirected {
             adv_data,
             scan_data,
         };
-        let conn = unwrap!(peripheral::advertise_connectable(sd, adv, &config).await);
 
-        info!("advertising done!");
+        let conn_future = peripheral::advertise_connectable(sd, adv, &config);
 
-        let res = gatt_server::run(&conn, server, |e| match e {
-            FooServiceEvent::FooWrite(val) => {
-                info!("wrote foo level: {}", val);
-                if let Err(e) = server.foo_notify(&conn, val + 1) {
-                    info!("send notification error: {:?}", e);
+        // instead of await to run one future, well select to run both futures until first one returns
+        let conn = futures::select_biased! {
+            // connection returns if somebody connects
+            conn = conn_future.fuse() => unwrap!(conn),
+            // button returns if pressed and well go back to top of loop
+            _ = button1.wait().fuse() => {info!("stopping"); continue 'waiting;},
+        };
+
+        let gatt_future = gatt_server::run(&conn, &server, |e| match e {
+            ServerEvent::Foo(e) => match e {
+                FooServiceEvent::FooWrite(val) => {
+                    info!("wrote foo: {}", val);
                 }
-            }
-            FooServiceEvent::FooNotificationsEnabled => info!("notifications enabled"),
-            FooServiceEvent::FooNotificationsDisabled => info!("notifications disabled"),
-        })
-        .await;
+            },
+        });
 
-        if let Err(e) = res {
-            info!("gatt_server run exited with error: {:?}", e);
-        }
+        // instead of await to run one future, well select to run both futures until first one returns
+        futures::select_biased! {
+            // gatt returns if connection drops
+            r = gatt_future.fuse() => info!("disconnected {}", r),
+            // button returns if pressed
+            _ = button1.wait().fuse() => info!("disconnecting"),
+        };
     }
 }
 
 #[embassy::task]
-async fn bluetooth_task(sd: &'static Softdevice, button1: AnyPin, button2: AnyPin) {
-    let server: FooService = unwrap!(gatt_server::register(sd));
-
-    info!("Bluetooth is OFF");
-    info!("Press microbit-v2 button 1 to enable, button 2 to disable");
-
-    let button1 = PortInput::new(Input::new(button1, Pull::Up));
-    let button2 = PortInput::new(Input::new(button2, Pull::Up));
-    pin_mut!(button1);
-    pin_mut!(button2);
-    loop {
-        button1.as_mut().wait_for_low().await;
-        info!("Bluetooth ON!");
-
-        // Create a future that will run the bluetooth loop.
-        // Note the lack of `.await`! This creates the future but doesn't poll it yet.
-        let bluetooth_fut = run_bluetooth(sd, &server);
-
-        // Create a future that will resolve when the OFF button is pressed.
-        let off_fut = async {
-            button2.as_mut().wait_for_low().await;
-            info!("Bluetooth OFF!");
-        };
-
-        pin_mut!(bluetooth_fut);
-        pin_mut!(off_fut);
-
-        // Select the two futures.
-        //
-        // select() returns when one of the two futures returns. The other future is dropped before completing.
-        //
-        // Since the bluetooth future never finishes, this can only happen when the Off button is pressed.
-        // This will cause the bluetooth future to be dropped.
-        //
-        // If it was advertising, the nested `peripheral::advertise_connectable` future will be dropped, which will cause
-        // the softdevice to stop advertising.
-        // If it was connected, it will drop everything including the `Connection` instance, which
-        // will tell the softdevice to disconnect it.
-        //
-        // This demonstrates the awesome power of Rust's async-await combined with nrf-softdevice's async wrappers.
-        // It's super easy to cancel a complex tree of operations: just drop its future!
-        futures::future::select(bluetooth_fut, off_fut).await;
-    }
+async fn softdevice_task(sd: &'static Softdevice) {
+    sd.run().await;
 }
 
-#[entry]
-fn main() -> ! {
-    info!("Hello World!");
-
-    let mut config = embassy_nrf::config::Config::default();
-    config.gpiote_interrupt_priority = Priority::P2;
-    config.time_interrupt_priority = Priority::P7;
-    let p = embassy_nrf::init(config);
-
-    let config = nrf_softdevice::Config {
+pub fn softdevice_config() -> nrf_softdevice::Config {
+    nrf_softdevice::Config {
         clock: Some(raw::nrf_clock_lf_cfg_t {
             source: raw::NRF_CLOCK_LF_SRC_RC as u8,
             rc_ctiv: 4,
@@ -158,30 +140,36 @@ fn main() -> ! {
             p_value: b"HelloRust" as *const u8 as _,
             current_len: 9,
             max_len: 9,
-            write_perm: unsafe { mem::zeroed() },
+            write_perm: unsafe { core::mem::zeroed() },
             _bitfield_1: raw::ble_gap_cfg_device_name_t::new_bitfield_1(
                 raw::BLE_GATTS_VLOC_STACK as u8,
             ),
         }),
         ..Default::default()
-    };
-
-    let sd = Softdevice::enable(&config);
-
-    let executor = EXECUTOR.put(Executor::new());
-    executor.run(|spawner| {
-        unwrap!(spawner.spawn(softdevice_task(sd)));
-        unwrap!(spawner.spawn(bluetooth_task(sd, p.P0_14.degrade(), p.P0_23.degrade())));
-    });
+    }
 }
 
-defmt::timestamp! {"{=u64}", {
+// 0 is Highest. Lower prio number can preempt higher prio number
+// Softdevice has reserved priorities 0, 1 and 3
+pub fn embassy_config() -> Config {
+    let mut config = Config::default();
+    config.hfclk_source = HfclkSource::ExternalXtal;
+    config.lfclk_source = LfclkSource::ExternalXtal;
+    // any reason not to run our timer as highest priority?
+    config.time_interrupt_priority = interrupt::Priority::P2;
+    // if we see button misses lower this
+    config.gpiote_interrupt_priority = interrupt::Priority::P7;
+    config
+}
+
+// WARNING may overflow and wrap-around in long lived apps
+defmt::timestamp! {"{=usize}", {
         use core::sync::atomic::{AtomicUsize, Ordering};
 
         static COUNT: AtomicUsize = AtomicUsize::new(0);
         // NOTE(no-CAS) `timestamps` runs with interrupts disabled
         let n = COUNT.load(Ordering::Relaxed);
         COUNT.store(n + 1, Ordering::Relaxed);
-        n as u64
+        n
     }
 }
