e95b4cb9d18bc1e03ae75eb4b983d2a6cd4f267b
Jacob Rosenthal <jacobrosenthal@gmail.com>
2021-10-22 11:32:58 -0700

    add a blinky task

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+//! https://tech.microbit.org/hardware/
+//!
 //! cargo run --release
 
 #![no_main]
@@ -9,10 +11,12 @@ use nrf_softdevice_defmt_rtt as _; // global logger
 use panic_probe as _;
 
 use embassy::executor::Spawner;
+use embassy::time::{Duration, Timer};
 use embassy_nrf::config::{Config, HfclkSource, LfclkSource};
 use embassy_nrf::gpio::{self, AnyPin, Pin};
 use embassy_nrf::gpiote::{self, AnyChannel, Channel, InputChannel};
 use embassy_nrf::{interrupt, Peripherals};
+use embedded_hal::digital::v2::OutputPin;
 use futures::FutureExt;
 use nrf_softdevice::ble::{gatt_server, peripheral};
 use nrf_softdevice::{raw, Softdevice};
@@ -42,8 +46,52 @@ async fn main(spawner: Spawner, dp: Peripherals) {
         gpiote::InputChannelPolarity::HiToLo,
     );
 
+    // microbit dosent have a single led, it has a matrix where you set the
+    // column high AND row low for the led you want to turn on.
+
+    // row1 permenantly powered
+    let _row1 = gpio::Output::new(
+        // degrade just a typesystem hack to forget which pin it is so we can
+        // call it Anypin and make our function calls more generic
+        dp.P0_21.degrade(),
+        gpio::Level::High,
+        gpio::OutputDrive::Standard,
+    );
+
+    // The column pins are active low, start leds high (off)
+    let red = gpio::Output::new(
+        dp.P0_28.degrade(),
+        gpio::Level::High,
+        gpio::OutputDrive::Standard,
+    );
+
     unwrap!(spawner.spawn(softdevice_task(sd)));
     unwrap!(spawner.spawn(bluetooth_task(sd, button1)));
+    unwrap!(spawner.spawn(blinky_task(red)));
+
+    // we can sneak another 'task' here as well
+    let mut red2 = gpio::Output::new(
+        dp.P0_11.degrade(),
+        gpio::Level::High,
+        gpio::OutputDrive::Standard,
+    );
+
+    loop {
+        unwrap!(red2.set_low());
+        Timer::after(Duration::from_millis(1000)).await;
+        unwrap!(red2.set_high());
+        Timer::after(Duration::from_millis(1000)).await;
+    }
+}
+
+#[embassy::task]
+async fn blinky_task(mut red: gpio::Output<'static, AnyPin>) {
+    loop {
+        unwrap!(red.set_high());
+        Timer::after(Duration::from_millis(1000)).await;
+        unwrap!(red.set_low());
+        Timer::after(Duration::from_millis(1000)).await;
+    }
 }
 
 #[embassy::task]
