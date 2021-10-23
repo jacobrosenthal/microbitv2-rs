b9a16ebb4d0f03ec4e7ce95f26f93298b24bed17
Jacob Rosenthal <jacobrosenthal@gmail.com>
2021-10-22 17:29:03 -0700

    lower softdevice memory usage

--- memory.x
+++ memory.x
@@ -1,7 +1,7 @@
 MEMORY
 {
   /* NOTE 1 K = 1 KiBi = 1024 bytes */
-  /* These values correspond to the NRF52833 with Softdevices S140 7.0.1 */
+  /* These values correspond to the NRF52833 with Softdevices S113 7.0.1 */
   FLASH : ORIGIN = 0x00000000 + 112K, LENGTH = 512K - 112K
-  RAM : ORIGIN = 0x20000000 + 64K, LENGTH = 128K - 64K
+  RAM : ORIGIN = 0x20000000 + 42K, LENGTH = 128K - 42K
 }
--- src/ble.rs
+++ src/ble.rs
@@ -95,14 +95,15 @@ pub async fn softdevice_task(sd: &'static Softdevice) {
 
 pub fn softdevice_config() -> nrf_softdevice::Config {
     nrf_softdevice::Config {
+        // todo shouldnt this be xtal?
         clock: Some(raw::nrf_clock_lf_cfg_t {
             source: raw::NRF_CLOCK_LF_SRC_RC as u8,
-            rc_ctiv: 4,
+            rc_ctiv: 16,
             rc_temp_ctiv: 2,
-            accuracy: 7,
+            accuracy: 0,
         }),
         conn_gap: Some(raw::ble_gap_conn_cfg_t {
-            conn_count: 6,
+            conn_count: 1,
             event_length: 24,
         }),
         conn_gatt: Some(raw::ble_gatt_conn_cfg_t { att_mtu: 256 }),
@@ -111,7 +112,7 @@ pub fn softdevice_config() -> nrf_softdevice::Config {
         }),
         gap_role_count: Some(raw::ble_gap_cfg_role_count_t {
             adv_set_count: 1,
-            periph_role_count: 3,
+            periph_role_count: 1,
         }),
         gap_device_name: Some(raw::ble_gap_cfg_device_name_t {
             p_value: b"HelloRust" as *const u8 as _,
@@ -130,8 +131,9 @@ pub fn softdevice_config() -> nrf_softdevice::Config {
 // Softdevice has reserved priorities 0, 1 and 3
 pub fn embassy_config() -> Config {
     let mut config = Config::default();
-    config.hfclk_source = HfclkSource::ExternalXtal;
-    config.lfclk_source = LfclkSource::ExternalXtal;
+    // ?
+    // config.hfclk_source = HfclkSource::ExternalXtal;
+    // config.lfclk_source = LfclkSource::ExternalXtal;
     // any reason not to run our timer as highest priority?
     config.time_interrupt_priority = interrupt::Priority::P2;
     // if we see button misses lower this
